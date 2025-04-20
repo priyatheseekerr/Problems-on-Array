9bd90f4b74af07abd8afcd264fe43354ce2f7333
theseekerrr <priyaseekerrr@gmail.com>
2025-04-20 13:08:12 +0530

    Reverse a given array

---
+++ ReverseArray.java
@@ -0,0 +1,21 @@
+//Reverse a given array
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the length of the array: ");
+        int len=sc.nextInt();
+
+        int[] arr=new int[len];
+        for(int i=0;i<len;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int j=len-1;j>=0;j--){
+            System.out.print(arr[j]+" ");
+        }
+        sc.close();
+    }
+}
