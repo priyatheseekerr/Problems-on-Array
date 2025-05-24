2da259fc0a0d16c92eb1d608861c7ac97308e9d9
theseekerrr <priyaseekerrr@gmail.com>
2025-05-24 20:10:53 +0530

    Left rotate array by one place

---
+++ LeftRotateByOne.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+//	Left rotate array by one place
+public class LeftRotateByOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the array : ");
+        int len = sc.nextInt();
+        int[] arr = new int[len];
+        System.out.print("Enter the array element:");
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
+        rotate(arr, len, 1);
+        sc.close();
+
+    }
+
+    static void rotate(int[] arr, int len, int k) {
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            result[i] = arr[(i + k) % len];
+        }
+        System.out.println(Arrays.toString(result));
+    }
+}
